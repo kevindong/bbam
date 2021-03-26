d735453edd256fb0421771cbb60eacf23a8384be
Kevin Dong <dong.kevin1@gmail.com>
2021-03-26 17:03:44 -0400

    Add ability to see number of ebikes by station and their charge levels

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ Originally this was intended to be a better bike angel (the branding name for Ci
 
 In early 2021, Lyft (the owner of the company operating Citibike) revamped the app and they made the new app harder to get info out of quicker (namely, info now pops up on the map as you pan around rather than having everything already being loaded immediately). So I've updated this app to give more info at a glance. Meaning, I now include the number of bikes available and number of docks available at each bike station.
 
+Additionally, ebikes are listed by charge levels.
+
+## Screenshot
+![BBAM Image](screenshot.png)
+
 ## Code Quality
 This app is not intended to be a shining example of code quality. In fact, this app was built as quickly as possible. 
 
--- browser.js
+++ browser.js
@@ -7,7 +7,7 @@ function initializeMap() {
       [40.86, -73.85],
     ]
   }).setView([40.7, -74.0], 14);
-  
+
   let tileUrl = 'https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=3c0d7c0f2de749deabab6bdf6b6dfbbb';
   let layer = new L.TileLayer(tileUrl, {
     maxZoom: 18,
@@ -27,14 +27,29 @@ async function refreshMarkers() {
     this.markerLayer.clearLayers();
   }
   this.markerLayer = new L.layerGroup().addTo(this.map);
-  
+
   let minimumPoints = document.getElementById("minimumPoints").value;
   let showLowPointStations = document.getElementById("showLowPointStations").checked;
   let data = await this.data;
   for (const point of data.features) {
     let bikesAvailable = point.properties.station.bikes_available;
     let docksAvailable = point.properties.station.docks_available;
-    let tooltipText = `${point.properties.station.name}<br>Bikes Available: ${bikesAvailable}<br>Docks Available: ${docksAvailable}`;
+    let ebikesAvailable = point.properties.bikes.length;
+
+    let tooltipText = `${point.properties.station.name}<br>Bikes Available: ${bikesAvailable}<br>Ebikes Available (total): ${ebikesAvailable}`;
+
+    let chargeLevelGroups = point.properties.bikes.reduce((a, c) => {
+      a[c.charge] += 1;
+      return a;
+    }, {1: 0, 2: 0, 3: 0, 4: 0});
+
+    for (let chargeLevel = 1; chargeLevel <= 4; chargeLevel++) {
+      if (chargeLevelGroups[chargeLevel] > 0) {
+        tooltipText += `<br>Ebikes (charge level ${chargeLevel}): ${chargeLevelGroups[chargeLevel]}`;
+      }
+    }
+
+    tooltipText += `<br><br>Docks Available: ${docksAvailable}`;
 
     let bikeAngelsAction = point.properties.bike_angels_action;
     let bikeAngelsPoints = point.properties.bike_angels_points || 0;
@@ -46,7 +61,7 @@ async function refreshMarkers() {
       continue;
     } else if (!showLowPointStations && bikeAngelsPoints < minimumPoints) {
       continue;
-    // The station has no incentive
+      // The station has no incentive
     } else if (!["give", "take"].includes(bikeAngelsAction)) {
       color = "gray-dot";
       text = "0";
@@ -57,7 +72,7 @@ async function refreshMarkers() {
       if (bikeAngelsAction === "give") {
         color += docksAvailable === 0 ? " red-border" : "";
         arrow = "⬇";
-      // Points are granted for a rider giving a bike / station taking a bike
+        // Points are granted for a rider giving a bike / station taking a bike
       } else {
         color += bikesAvailable === 0 ? " red-border" : "";
         arrow = "⬆";
@@ -65,11 +80,17 @@ async function refreshMarkers() {
       text = `${arrow} ${bikeAngelsPoints}`;
     }
 
-    text += `<br/>B${bikesAvailable} - D${docksAvailable}`;
+    text += `<br/>B${bikesAvailable} - `;
+    if (ebikesAvailable > 0) {
+      text += `E${ebikesAvailable}<br>`;
+    }
+    text += `D${docksAvailable}`
 
-    L.marker(point.geometry.coordinates.sort().reverse(), {icon: new L.DivIcon({
-      className: color,
-      html: `<div style=\"text-align: center; margin: 5px; white-space: nowrap;\">${text}</div>`
-    })}).bindTooltip(tooltipText).addTo(this.markerLayer);
+    L.marker(point.geometry.coordinates.sort().reverse(), {
+      icon: new L.DivIcon({
+        className: color,
+        html: `<div style=\"text-align: center; margin: 5px; white-space: nowrap;\">${text}</div>`
+      })
+    }).bindTooltip(tooltipText).addTo(this.markerLayer);
   }
 }
